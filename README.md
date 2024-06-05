a103826422759a36398f0c5e2f8e7bcf2a387b6a
crunny <70722011+crunny@users.noreply.github.com>
2024-06-05 01:17:09 +0100

    type shi

---
+++ README.md
@@ -0,0 +1,39 @@
+# YouTube Downloader
+
+Simple YouTube downloader application built using Python. It allows you to download videos and audio from YouTube. The application has a GUI made with customtkinter and a custom theme. (Downloads stored in "~/Downloads" by default)
+
+## Features
+
+- Download YouTube videos and audio.
+- Progress bar to show download status.
+- Merges video and audio streams for better quality.
+
+## Usage:
+
+1. **Clone the repository:**
+
+   `git clone https://github.com/crunny/youtube-downloader.git`
+
+   `cd youtube-downloader`
+
+2. **Install the dependencies:**
+
+   `pip install -r requirements.txt`
+
+3. **Run it:**
+
+   `python main.py`
+
+4. **Enter the YouTube URL and select the format (Video or Audio).**
+
+5. **Click the "Download" button and wait for the download to complete.**
+
+## Legal Disclaimer
+
+This repository and its contents are provided for educational and informational purposes only. The code and resources shared here are not intended for production use without proper testing and validation.
+
+The author makes no representations or warranties regarding the accuracy, completeness, or suitability of the information shared. Any reliance you place on such information is therefore strictly at your own risk.
+
+In no event shall the author be liable for any special, direct, indirect, or consequential damages or any damages whatsoever resulting from loss of use, data, or profits, arising out of or in connection with the use or performance of any information shared on this repository.
+
+You are solely responsible for determining the appropriateness of using and/or modifying any code shared here, and you agree to comply with all applicable laws and regulations. Use of the code and resources shared in this repository constitutes your acceptance of these terms.
---
+++ main.py
@@ -0,0 +1,146 @@
+import os
+from pytube import YouTube
+from pytube.exceptions import PytubeError
+import customtkinter as ctk
+from moviepy.video.io.ffmpeg_tools import ffmpeg_merge_video_audio
+
+output_folder_path = os.path.join(os.path.expanduser("~"), "Downloads")
+
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("theme.json")
+
+root = ctk.CTk()
+root.title("YouTube Downloader")
+root.geometry("600x450")
+root.resizable(False, False)
+
+font_size = 16
+
+url_label = ctk.CTkLabel(root, text="Enter YouTube URL:", font=("Poppins", font_size))
+url_label.pack(pady=20)
+
+url_entry = ctk.CTkEntry(root, width=400, font=("Poppins", font_size))
+url_entry.pack(pady=10)
+
+format_label = ctk.CTkLabel(root, text="Choose format:", font=("Poppins", font_size))
+format_label.pack(pady=20)
+
+format_var = ctk.StringVar(value="video")
+
+video_radio = ctk.CTkRadioButton(
+    root, text="Video", variable=format_var, value="Video", font=("Poppins", font_size)
+)
+video_radio.pack(pady=0)
+
+audio_radio = ctk.CTkRadioButton(
+    root, text="Audio", variable=format_var, value="Audio", font=("Poppins", font_size)
+)
+audio_radio.pack(pady=10)
+
+progress_bar = ctk.CTkProgressBar(root, width=400)
+progress_bar.pack(pady=20)
+progress_bar.set(0)
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    new_percentage = bytes_downloaded / total_size * 100
+
+    current_percentage = progress_bar.get() * 100
+
+    increment_size = (new_percentage - current_percentage) / 10
+
+    for _ in range(10):
+        current_percentage += increment_size
+        progress_bar.set(current_percentage / 100)
+        status_label.configure(text=f"Downloading... {int(current_percentage)}%")
+        root.update()
+
+    progress_bar.set(new_percentage / 100)
+    status_label.configure(text=f"Downloading... {int(new_percentage)}%")
+
+
+def download():
+    url = url_entry.get()
+    format = format_var.get()
+    try:
+        yt = YouTube(url, on_progress_callback=on_progress)
+        if format == "Audio":
+            audio_streams = yt.streams.filter(only_audio=True).order_by("abr").desc()
+            if audio_streams:
+                audio = audio_streams.first()
+                output_file_path = os.path.join(
+                    output_folder_path, audio.default_filename
+                )
+                if os.path.exists(output_file_path):
+                    status_label.configure(text="File already exists!")
+                else:
+                    status_label.configure(text="Downloading audio...")
+                    audio.download(output_path=output_folder_path)
+                    status_label.configure(text="Downloaded successfully!")
+            else:
+                status_label.configure(text="No audio streams available!")
+        elif format == "Video":
+            video_streams = (
+                yt.streams.filter(type="video", progressive=False, file_extension="mp4")
+                .order_by("resolution")
+                .desc()
+            )
+            audio_streams = (
+                yt.streams.filter(only_audio=True, file_extension="mp4")
+                .order_by("abr")
+                .desc()
+            )
+
+            if video_streams and audio_streams:
+                video = video_streams.first()
+                audio = audio_streams.first()
+
+                video_output_path = os.path.join(output_folder_path, "video.mp4")
+                audio_output_path = os.path.join(output_folder_path, "audio.mp4")
+
+                try:
+                    status_label.configure(text="Downloading video...")
+                    video.download(output_path=output_folder_path, filename="video.mp4")
+
+                    status_label.configure(text="Downloading audio...")
+                    audio.download(output_path=output_folder_path, filename="audio.mp4")
+
+                    final_output_path = os.path.join(
+                        output_folder_path, f"{yt.title}.mp4"
+                    )
+
+                    status_label.configure(text="Merging video and audio...")
+                    ffmpeg_merge_video_audio(
+                        video_output_path, audio_output_path, final_output_path
+                    )
+                finally:
+                    if os.path.exists(video_output_path):
+                        os.remove(video_output_path)
+                    if os.path.exists(audio_output_path):
+                        os.remove(audio_output_path)
+
+                status_label.configure(text="Downloaded successfully!")
+            else:
+                status_label.configure(text="No video/audio streams available!")
+    except PytubeError as e:
+        status_label.configure(text=f"Pytube Error: {e}")
+    except OSError as e:
+        status_label.configure(text=f"OS Error: {e}")
+    except Exception as e:
+        status_label.configure(text=f"Error: {e}")
+
+
+download_button = ctk.CTkButton(
+    root,
+    text="Download",
+    font=("Poppins", font_size),
+    command=download,
+)
+download_button.pack(pady=10)
+
+status_label = ctk.CTkLabel(root, text="", font=("Poppins", font_size))
+status_label.pack(pady=10)
+
+root.mainloop()
